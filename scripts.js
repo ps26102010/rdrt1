b4ff8a74979fa1a1b32b675374aa26a47858e8f1
ps26102010 <163730755+ps26102010@users.noreply.github.com>
2024-06-14 12:57:14 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ function checkAnswers() {
     var answer1 = document.getElementById("answer1").value.trim().toUpperCase();
 
     if (answer1 === "AJITESH") {
-        window.location.href = "https://cysx.000webhostapp.com/";
+        window.location.href = "https://ps26102010.github.io/cysx/";
     } else {
         alert("youre very close, try not to die <3");
     }
